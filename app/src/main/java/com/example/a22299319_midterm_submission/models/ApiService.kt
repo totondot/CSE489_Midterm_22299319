e079b533df2f98d5fdc72e650d77e4a313ef16a5
totondot <abrar.zahin.office@gmail.com>
2025-12-10 20:45:15 +0600

    feat: implemented update landmark functionality

--- app/src/main/java/com/example/a22299319_midterm_submission/models/ApiService.kt
+++ app/src/main/java/com/example/a22299319_midterm_submission/models/ApiService.kt
@@ -4,9 +4,12 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.DELETE
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Query
 
@@ -25,7 +28,15 @@ interface ApiService {
         @Part("lon") lon: RequestBody,
         @Part image: MultipartBody.Part
     ): Call<Any> // Response is a generic JSON object
-
+    // 4. UPDATE Request (Text only for stability)
+    @FormUrlEncoded
+    @PUT("api.php")
+    fun updateLandmark(
+        @Field("id") id: Int,
+        @Field("title") title: String,
+        @Field("lat") lat: Double,
+        @Field("lon") lon: Double
+    ): Call<Any>
     // Delete [cite: 132]
     @DELETE("api.php")
     fun deleteLandmark(@Query("id") id: Int): Call<Void>
